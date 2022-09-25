cdcbf1f4551458d6ab5cf8e486a62792dfd856f8
Alexander Iordanou <iordanoualexander@gmail.com>
2022-09-25 17:25:47 +0300

    Modifies models to rename f_name to name and remove l_name. Applies migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -10,43 +10,43 @@ from django.contrib.auth.hashers import make_password
 class UserProfileManager(BaseUserManager, models.Manager):
     #Manager for user profiles
 
-    def create_user(self, email, f_name, password=None):
+    def create_user(self, email, name, password=None):
         # Create a new user profile
         if not email:
             raise ValueError('Users must have an E-Mail address')
         
         email = self.normalize_email(email)
-        user = self.model(email=email, name=f_name)
+        user = self.model(email=email, name=name)
 
-        user.make_password(password)
+        user.set_password(password)
         user.save(using=self._db)
         
 
         return user
 
-    def create_superuser(self, email, f_name, password):
+    def create_superuser(self, email, name, password):
 
-        user = self.create_user(email, f_name, password)
+        user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
+        user.save(using=self._db)
 
-
+        return user
 class UserProfile(AbstractBaseUser, PermissionsMixin, models.Model):
     #database model for users in the system
 
     email = models.EmailField(unique=True)
-    f_name = models.CharField(max_length=50)
-    l_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email' #this will replace the default username with email in authentication
-    REQUIRED_FIELDS = ['f_name', 'l_name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        return self.f_name + " " + self.l_name
+        return self.name
 
     def __str__(self):
         return self.email
