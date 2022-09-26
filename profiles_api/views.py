c4357560fc68e93d5346f222ec2afd39c4c2b505
Alexander Iordanou <iordanoualexander@gmail.com>
2022-09-26 20:44:57 +0300

    creates user profile modelviewset and configures router

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import UserProfile
+from rest_framework.viewsets import ModelViewSet
+from .serializers import UserProfileSerializer
 # Create your views here.
+
+
+class UserProfileViewSet(ModelViewSet):
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from profiles_api.views import UserProfileViewSet
+
+
+router = DefaultRouter()
+router.register('profiles', UserProfileViewSet),
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
